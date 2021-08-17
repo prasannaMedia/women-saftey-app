f64db2efc143dff8f3962c84cc681ca61f41419e
prasanna <67369708+prasannaMedia@users.noreply.github.com>
2021-08-17 10:41:13 +0530

    i am sorry

--- app/src/main/java/com/dan/naari/AddRelative.java
+++ app/src/main/java/com/dan/naari/AddRelative.java
@@ -1,9 +1,16 @@
 package com.dan.naari;
-
+import  java.lang.String;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.content.Context;
+import android.annotation.SuppressLint;
+import android.location.Location;
+import android.location.LocationManager;
+import androidx.annotation.NonNull;
+import android.os.Looper;
+import android.provider.Settings;
 import android.database.sqlite.SQLiteOpenHelper;
 //import android.support.v7.app.AppCompatActivity;
 import android.net.Uri;
@@ -18,24 +25,31 @@ import android.widget.ListAdapter;
 import android.widget.Toast;
 import android.telephony.SmsManager;
 import android.app.PendingIntent;
-
-
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.regex.Pattern;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class AddRelative extends AppCompatActivity {
 
     private static final int REQUEST_CALL = 1;
     private static final int REQUEST_SMS = 1;
+    double latitude;
+    double longitude;
     DatabaseHelper myDB;
     Button btnAdd,btnView;
     EditText editText, editText2;
+    FusedLocationProviderClient mFusedLocationClient;
+    int PERMISSION_ID = 44;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +60,8 @@ public class AddRelative extends AppCompatActivity {
         btnAdd = (Button) findViewById(R.id.btnAdd);
         btnView = (Button) findViewById(R.id.btnView);
         myDB = new DatabaseHelper(this);
-
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        getLastLocation();
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,6 +88,117 @@ public class AddRelative extends AppCompatActivity {
 
 
     }
+    @SuppressLint("MissingPermission")
+    private void getLastLocation() {
+        // check if permissions are given
+        if (checkPermissions()) {
+
+            // check if location is enabled
+            if (isLocationEnabled()) {
+
+                // getting last
+                // location from
+                // FusedLocationClient
+                // object
+                mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        Location location = task.getResult();
+                        if (location == null) {
+                            requestNewLocationData();
+                        } else {
+                           latitude= location.getLatitude();
+                           longitude=location.getLongitude();
+                            System.out.println(latitude);
+                            System.out.println(longitude);
+                        }
+                    }
+                });
+            } else {
+                Toast.makeText(this, "Please turn on" + " your location...", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        } else {
+            // if permissions aren't available,
+            // request for permissions
+            requestPermissions();
+        }
+    }
+    @SuppressLint("MissingPermission")
+    private void requestNewLocationData() {
+
+        // Initializing LocationRequest
+        // object with appropriate methods
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(5);
+        mLocationRequest.setFastestInterval(0);
+        mLocationRequest.setNumUpdates(1);
+
+        // setting LocationRequest
+        // on FusedLocationClient
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+    }
+
+    private LocationCallback mLocationCallback = new LocationCallback() {
+
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            Location mLastLocation = locationResult.getLastLocation();
+            latitude = mLastLocation.getLatitude();
+            longitude = mLastLocation.getLongitude();
+            System.out.println(latitude);
+            System.out.println(longitude);
+        }
+    };
+
+    // method to check for permissions
+    private boolean checkPermissions() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+
+        // If we want background location
+        // on Android 10.0 and higher,
+        // use:
+        // ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    // method to request for permissions
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_ID);
+    }
+
+    // method to check
+    // if location is enabled
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    // If everything is alright then
+    @Override
+    public void
+    onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (checkPermissions()) {
+            getLastLocation();
+        }
+    }
+
 
     public void AddData(String newEntry) {
 
@@ -130,12 +256,12 @@ public class AddRelative extends AppCompatActivity {
                             Intent intent=new Intent(getApplicationContext(),AddRelative.class);
                             PendingIntent pi=PendingIntent.getActivity(getApplicationContext(), 0, intent,0);
                             SmsManager sms=SmsManager.getDefault();
-                            sms.sendTextMessage(Number.trim(), null, "hello i am in emergency situation my location is lat lng", pi,null);
+                            sms.sendTextMessage(Number.trim(), null, "Hello i am in emergency situation my location is: https://www.google.com/maps/search/?api=1&query="+latitude+","+longitude, pi,null);
                         } else {
                             Intent intent=new Intent(getApplicationContext(),AddRelative.class);
                             PendingIntent pi=PendingIntent.getActivity(getApplicationContext(), 0, intent,0);
                             SmsManager sms=SmsManager.getDefault();
-                            sms.sendTextMessage(Number.trim(), null, "hello i am in emergency situation my location is lat lng", pi,null);
+                            sms.sendTextMessage(Number.trim(), null, "Hello i am in emergency situation my location is: https://www.google.com/maps/search/?api=1&query="+latitude+","+longitude, pi,null);
                         }
                         startActivity(callIntent);
                         counter +=2;
--- app/src/main/java/com/dan/naari/Login.java
+++ app/src/main/java/com/dan/naari/Login.java
@@ -19,7 +19,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class ogin extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     EditText mEmail,mPassword;
     Button mLoginBtn;
